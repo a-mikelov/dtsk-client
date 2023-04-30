cc02cd07c5810870a8c8f1aca0a0cc96824746f9
a.mayatskiy <work.mayatskiy@gmail.com>
2023-04-30 10:12:17 +1000

    fix: update styles for services block

--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -2,10 +2,17 @@ import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
 import {ServicesComponent} from './services.component'
 import {TuiButtonModule, TuiLoaderModule, TuiSvgModule} from '@taiga-ui/core'
+import {TuiLetModule} from '@taiga-ui/cdk'
 
 @NgModule({
   declarations: [ServicesComponent],
   exports: [ServicesComponent],
-  imports: [CommonModule, TuiButtonModule, TuiSvgModule, TuiLoaderModule],
+  imports: [
+    CommonModule,
+    TuiButtonModule,
+    TuiSvgModule,
+    TuiLoaderModule,
+    TuiLetModule,
+  ],
 })
 export class ServicesModule {}
