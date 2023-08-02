f227043108eafe686a1d1e5dff4b452a74912743
a.mayatskiy <work.mayatskiy@gmail.com>
2023-08-02 13:36:46 +1000

    fix: add scroll for page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import {NgModule} from '@angular/core'
-import {RouterModule, Routes} from '@angular/router'
+import {ExtraOptions, RouterModule, Routes} from '@angular/router'
+
+const routerOptions: ExtraOptions = {
+  scrollOffset: [0, 0],
+  scrollPositionRestoration: 'enabled',
+}
 
 const routes: Routes = []
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, routerOptions)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
